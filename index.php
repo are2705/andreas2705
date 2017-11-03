4294024ae2d89c8760a87286ed3ee77bc88889f0
are2705 <33349265+are2705@users.noreply.github.com>
2017-11-03 14:48:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <html>
   <head>
+	  <meta charset="UTF-8" />
+	  <title>Testseite</title> 
   </head>
   <body>
     <h1>Hey</h1>
-    <p>Läuft...</p>
+    
+    <form action="backend.php" method="post">
+      <p>Benutzer:<br /><imput type="Text" name="user" /></p>
+      <p>Passwort:<br /><input type="Password" name="passwort" /></p>
+      <p><input type="Submit" value="Absenden" /></p>
+    </form>
+    
     <?php
       echo "<p>Es ist ".date("H:i")." Uhr.</p>"
     ?>
